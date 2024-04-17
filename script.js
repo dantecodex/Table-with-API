5d80062c31803ad01e281ce887857a23e701e482
dantecodex <anshulrajput237@gmail.com>
2024-04-17 16:32:17 +0530

    method 1 done

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const columnList = ['brand', 'title', 'price', 'rating', 'stock'];
 const showItem = [3, 6, 9, 12, 15];
 const apiURL = 'https://dummyjson.com/products';
-const productPerPage = 15;
+const productPerPage = 5;
 let currentPage = 1;
 
 boostrapTable();
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Sort Product
     const sortList = document.querySelectorAll(".sort-Content li");
     sortList.forEach(list => {
         list.addEventListener("click", event => {
@@ -15,41 +14,43 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Show Hide Product
-    const showProduct = document.querySelectorAll(".show-Content li");
-    showProduct.forEach(content => {
-        content.addEventListener("click", event => {
-            let showProductNumber = event.target.textContent;
-            const allProduct = document.querySelectorAll(".primary-table > tbody > tr");
-
-            for (let index = 0; index < Number(showProductNumber); index++) {
-                const element = allProduct[index];
-                element.classList.remove("display-none");
-            }
-            for (let index = Number(showProductNumber); index < allProduct.length; index++) {
-                const element = allProduct[index];
-                element.classList.add("display-none");
-            }
-        })
-    });
-
-    // Page Navigation
-    let totalPages = Math.ceil(getSetData().length / 15);
-    let localData = getSetData();
     document.querySelector(".prevPage").addEventListener("click", event => {
-        if (currentPage > 1) {
-            currentPage--;
-            drawTable(localData);
-        }
+
+        currentPage--;
+        searchProducts();
+
     });
     document.querySelector(".nextPage").addEventListener("click", event => {
-        if (currentPage < totalPages) {
-            currentPage++
-            drawTable(localData);
-        }
+
+        currentPage++
+        searchProducts();
+
     });
 
 })
+
+function paginate(result) {
+    let totalProduct = result.length;
+    let totalPages = totalProduct / productPerPage;
+
+    let startIndex = (currentPage - 1) * productPerPage;
+    let endIndex = startIndex + productPerPage;
+
+    document.querySelector(".nextPage").style.display = "block";
+    document.querySelector(".prevPage").style.display = "block";
+
+    if (currentPage == totalPages) {
+        document.querySelector(".nextPage").style.display = "none";
+
+    }
+    if (currentPage < 2) {
+        document.querySelector(".prevPage").style.display = "none";
+
+    }
+    result = result.slice(startIndex, endIndex);
+    drawTable(result);
+
+}
 
 
 function boostrapTable() {
@@ -70,8 +71,8 @@ function boostrapTable() {
     sortContent.innerHTML = sortList;
     showContent.innerHTML = showList;
 
-
     inputBox.addEventListener("input", event => {
+        currentPage = 1;
         searchProducts();
     });
 
@@ -80,10 +81,11 @@ function boostrapTable() {
 
 async function loadTable() {
     let products = await fetchAPI();
-    drawTable(products);
+    paginate(products);
 }
 
-function searchProducts(category) {
+function searchProducts(category = null) {
+
     let inputValue = document.getElementById("searchInput").value;
     inputValue = inputValue.trim().toLowerCase();
     const localData = getSetData();
@@ -101,17 +103,9 @@ function searchProducts(category) {
     });
     if (!result.length && !inputValue)
         result = localData;
-    if (!result.length && inputValue)
-        result = [{
-            "title": "NO DATA",
-            "description": "NO DATA",
-            "price": 'NO DATA',
-            "rating": 'NO DATA',
-            "stock": 'NO DATA',
-            "brand": "NO DATA"
-        }]
 
-    drawTable(result);
+
+
     sortContent(category, result);
 }
 
@@ -131,43 +125,27 @@ function sortContent(category, result) {
 
             break;
     }
-    drawTable(result);
+    paginate(result);
 }
 
 
 function drawTable(products) {
     const tableBody = document.querySelector(".primary-table tbody");
     let tableRows = ``;
-    let startIndex = (currentPage - 1) * productPerPage;
-    let lastIndex = startIndex + productPerPage;
-    if (products.length > productPerPage) {
-        for (let index = startIndex; index < lastIndex; index++) {
-            let product = products[index]
-            tableRows += `
+
+    products = products.length == 0 ? [{}] : products;
+    for (const product of products) {
+        tableRows += `
             <tr>
-            <td data-cell="brand">${product.brand}</td>
-            <td data-cell="title">${product.title}</td>
-            <td data-cell="description">${product.description}</td>
-            <td data-cell="price">$${product.price}</td>
-            <td data-cell="rating">${product.rating}</td>
-            <td data-cell="stock">${product.stock}</td>
+            <td data-cell="brand">${product.id ?? 'NO DATA'}</td>
+            <td data-cell="brand">${product.brand ?? 'NO DATA'}</td>
+            <td data-cell="title">${product.title ?? 'NO DATA'}</td>
+            <td data-cell="description">${product.description ?? 'NO DATA'}</td>
+            <td data-cell="price">${product.price ? '$ ' + product.price : 'NO DATA'}</td>
+            <td data-cell="rating">${product.rating ?? 'NO DATA'}</td>
+            <td data-cell="stock">${product.stock ?? 'NO DATA'}</td>
             </tr>
             `;
-        }
-    }
-    else {
-        for (const product of products) {
-            tableRows += `
-            <tr>
-            <td data-cell="brand">${product.brand}</td>
-            <td data-cell="title">${product.title}</td>
-            <td data-cell="description">${product.description}</td>
-            <td data-cell="price">$${product.price}</td>
-            <td data-cell="rating">${product.rating}</td>
-            <td data-cell="stock">${product.stock}</td>
-            </tr>
-            `;
-        }
     }
     tableBody.innerHTML = tableRows;
 }
